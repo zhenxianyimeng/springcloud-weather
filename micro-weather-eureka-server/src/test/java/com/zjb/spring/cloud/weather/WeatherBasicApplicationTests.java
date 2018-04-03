7705e03d749dcd21d22dbee994b5202d3748872f
zhenxianyimeng <1920405993@qq.com>
2018-04-03 10:27:24 +0800

    增加eureka工程

---
+++ micro-weather-eureka-server/src/test/java/com/zjb/spring/cloud/weather/WeatherBasicApplicationTests.java
@@ -0,0 +1,20 @@
+package com.zjb.spring.cloud.weather;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherBasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		List<String> list = new ArrayList<>();
+	}
+
+}
