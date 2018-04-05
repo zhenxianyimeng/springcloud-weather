7d7be76605d8265b6ca5bdde1f58da8bd2e95a1e
zhenxianyimeng <1920405993@qq.com>
2018-04-05 11:14:22 +0800

    几个拆分的服务引入eureka client

--- msa-weather-collection-server/src/main/java/com/zjb/spring/cloud/weather/MsaWeatherCollectionApplication.java
+++ msa-weather-collection-server/src/main/java/com/zjb/spring/cloud/weather/MsaWeatherCollectionApplication.java
@@ -2,8 +2,10 @@ package com.zjb.spring.cloud.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsaWeatherCollectionApplication {
 
 	public static void main(String[] args) {
