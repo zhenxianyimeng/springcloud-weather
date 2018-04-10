2c27b4a7615b8fc31c66c19c3363b6aaad5e325b
zhenxianyimeng <1920405993@qq.com>
2018-04-10 23:34:00 +0800

    修改部分接口

--- msa-weather-city-server/src/main/java/com/zjb/spring/cloud/weather/MsaWeatherCityApplication.java
+++ msa-weather-city-server/src/main/java/com/zjb/spring/cloud/weather/MsaWeatherCityApplication.java
@@ -2,8 +2,10 @@ package com.zjb.spring.cloud.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsaWeatherCityApplication {
 
 	public static void main(String[] args) {
